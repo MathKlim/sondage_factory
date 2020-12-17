ad3e90bfeba3832f43303de55f4d1dfd904285cd
vorph <klimczak.mathieu@pm.me>
2020-12-17 11:00:22 +0100

    passage a openpyxl pour lire les excels

--- sondage.py
+++ sondage.py
@@ -1,17 +1,39 @@
+from itertools import islice
+
 import numpy as np
 import pandas as pd
 import streamlit as st
+from openpyxl import load_workbook
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
 st.sidebar.image("logo/Logo_CITC_Gris.png", use_column_width=True)
 
-df = pd.read_excel("Sondage.xlsx")
+wb = load_workbook(filename="Sondage.xlsx")
+ws = wb.active
+data = ws.values
+cols = next(data)[1:]
+data = list(data)
+idx = [r[0] for r in data]
+data = (islice(r, 1, None) for r in data)
+# df = pd.read_excel("Sondage.xlsx", engine="openpyxl")
+
+df = pd.DataFrame(data, index=idx, columns=cols)
 df = df.iloc[1:]
 
-df
 
-df_nl = pd.read_excel("Sondage_NL.xlsx")
+wb2 = load_workbook(filename="Sondage_NL.xlsx")
+ws2 = wb2.active
+data2 = ws2.values
+cols2 = next(data2)[1:]
+data2 = list(data2)
+idx2 = [r[0] for r in data2]
+data2 = (islice(r, 1, None) for r in data2)
+# df = pd.read_excel("Sondage.xlsx", engine="openpyxl")
+
+df_nl = pd.DataFrame(data2, index=idx2, columns=cols2)
+
+# df_nl = pd.read_excel("Sondage_NL.xlsx", engine="openpyxl")
 
 st.title("Résumé du sondage factory 4.0")
 
@@ -21,14 +43,14 @@ cols1 = [
     if (c.lower()[:6] != "points" and c.lower()[:8] != "feedback")
 ]
 
-df_redux = df[cols1[7:]]
+df_redux = df[cols1[6:]]
 
 cols2 = [
     c
     for c in df_nl.columns
     if (c.lower()[:6] != "points" and c.lower()[:8] != "feedback")
 ]
-df_nl_redux = df_nl[cols2[7:]]
+df_nl_redux = df_nl[cols2[6:]]
 
 fr = st.sidebar.checkbox("Sondage francophone", value=True)
 
